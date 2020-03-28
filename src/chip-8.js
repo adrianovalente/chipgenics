d055a7a048fa2e9093566473bbedd868ad8bbea2
adrianovalente <dri9595@gmail.com>
2020-03-28 16:20:55 -0300

    Housekeeping 💅

--- src/chip-8.js
+++ src/chip-8.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-case-declarations */
 
 const CHIP_8_MEMORY_LENGTH = 4096
-// const CHIP_8_STACK_LENGTH = 16
 const CHIP_8_REGISTERS_LENGTH = 16
 const CHIP_8_VF_INDEX = 0xf
 
@@ -10,10 +9,10 @@ const OpCodes = {
   JUMP_IF_MATCHES_VALUE: 3,
   JUMP_IF_DOES_NOT_MATCH_VALUE: 4,
   JUMP_IF_MATCHES_REGISTER: 5,
-  CONDITIONAL_JUMP: 9,
   LOAD_VALUE: 6,
   ADD_VALUE: 7,
-  BIT_OPERATIONS: 8
+  BIT_OPERATIONS: 8,
+  JUMP_IF_DOES_NOT_MATCH_REGISTER: 9
 }
 
 class Chip8 {
@@ -57,10 +56,11 @@ class Chip8 {
    */
   _execute () {
     const instruction = this.memory[this.pc]
-    let diff, borrow // 🌈
+    let sum, diff, borrow // 🌈
 
     const x = (instruction & 0x0f00) >> 8
     const y = (instruction & 0x00f0) >> 4
+    const value = instruction & 0x00ff
 
     if (this.debug) {
       console.log(`PC: 0x${this.pc.toString(16)}, executing instruction: 0x${instruction.toString(16)}`)
@@ -71,91 +71,76 @@ class Chip8 {
         this.pc = instruction & 0x0fff
         return this
 
-      case OpCodes.CONDITIONAL_JUMP:
-        return this.registers[(instruction & 0x0f00) >> 8] !== this.registers[(instruction & 0x00f0) >> 4]
-          ? this._incrementProgramCounter()._incrementProgramCounter()
-          : this._incrementProgramCounter()
-
-      case OpCodes.JUMP_IF_MATCHES_VALUE:
-        return this.registers[x] === (instruction & 0x00ff)
-          ? this._incrementProgramCounter()._incrementProgramCounter()
-          : this._incrementProgramCounter()
-
-      case OpCodes.JUMP_IF_DOES_NOT_MATCH_VALUE:
-        return this.registers[x] !== (instruction & 0x00ff)
-          ? this._incrementProgramCounter()._incrementProgramCounter()
-          : this._incrementProgramCounter()
-
-      case OpCodes.JUMP_IF_MATCHES_REGISTER:
-        return this.registers[x] === this.registers[y]
-          ? this._incrementProgramCounter()._incrementProgramCounter()
-          : this._incrementProgramCounter()
+      case OpCodes.JUMP_IF_DOES_NOT_MATCH_REGISTER: return this._jumpIf(this.registers[x] !== this.registers[y])
+      case OpCodes.JUMP_IF_MATCHES_VALUE: return this._jumpIf(this.registers[x] === (instruction & 0x00ff))
+      case OpCodes.JUMP_IF_DOES_NOT_MATCH_VALUE: return this._jumpIf(this.registers[x] !== (instruction & 0x00ff))
+      case OpCodes.JUMP_IF_MATCHES_REGISTER: return this._jumpIf(this.registers[x] === this.registers[y])
 
       case OpCodes.LOAD_VALUE:
-        this.registers[(instruction & 0x0f00) >> 8] = (instruction & 0x00ff)
+        this.registers[x] = value
         return this._incrementProgramCounter()
 
       case OpCodes.ADD_VALUE:
-        const sum = this.registers[(instruction & 0x0f00) >> 8] + (instruction & 0x00ff)
+        sum = this.registers[x] + value
 
-        this.registers[(instruction & 0x0f00) >> 8] = sum % 0x100
+        this.registers[x] = sum % 0x100
         return this._incrementProgramCounter()
 
       case OpCodes.BIT_OPERATIONS:
         switch (instruction & 0x000f) {
           // store
           case 0:
-            this.registers[(instruction & 0x0f00) >> 8] = this.registers[(instruction & 0x00f0) >> 4]
+            this.registers[x] = this.registers[y]
             return this._incrementProgramCounter()
 
           case 1:
-            this.registers[(instruction & 0x0f00) >> 8] = this.registers[(instruction & 0x0f00) >> 8] | this.registers[(instruction & 0x00f0) >> 4]
+            this.registers[x] = this.registers[x] | this.registers[y]
             return this._incrementProgramCounter()
 
           case 2:
-            this.registers[(instruction & 0x0f00) >> 8] = this.registers[(instruction & 0x0f00) >> 8] & this.registers[(instruction & 0x00f0) >> 4]
+            this.registers[x] = this.registers[x] & this.registers[y]
             return this._incrementProgramCounter()
 
           case 3:
-            this.registers[(instruction & 0x0f00) >> 8] = this.registers[(instruction & 0x0f00) >> 8] ^ this.registers[(instruction & 0x00f0) >> 4]
+            this.registers[x] = this.registers[x] ^ this.registers[y]
             return this._incrementProgramCounter()
 
           case 4:
-            const sum = this.registers[(instruction & 0x0f00) >> 8] + this.registers[(instruction & 0x00f0) >> 4]
+            sum = this.registers[x] + this.registers[y]
 
             // setting carry to VF
-            this.registers[CHIP_8_VF_INDEX] = sum > 0xff ? 1 : 0
+            this.registers[CHIP_8_VF_INDEX] = sum > 0x00ff ? 1 : 0
 
             // taking care that maybe the result is higher than 256
-            this.registers[(instruction & 0x0f00) >> 8] = sum % 0x100
+            this.registers[(instruction & 0x0f00) >> 8] = sum % 0x0100
             return this._incrementProgramCounter()
 
           case 5:
-            diff = this.registers[(instruction & 0x0f00) >> 8] - this.registers[(instruction & 0x00f0) >> 4]
+            diff = this.registers[x] - this.registers[y]
             borrow = diff < 0
 
-            this.registers[(instruction & 0x0f00) >> 8] = borrow ? diff + 0x100 : diff
+            this.registers[x] = borrow ? diff + 0x0100 : diff
             this.registers[CHIP_8_VF_INDEX] = borrow ? 0 : 1
 
             return this._incrementProgramCounter()
 
           case 6:
-            this.registers[CHIP_8_VF_INDEX] = this.registers[(instruction & 0x00f0) >> 4] & 0x0001
-            this.registers[(instruction & 0x0f00) >> 8] = this.registers[(instruction & 0x00f0) >> 4] >> 1
+            this.registers[CHIP_8_VF_INDEX] = this.registers[y] & 0x0001
+            this.registers[x] = this.registers[y] >> 1
             return this._incrementProgramCounter()
 
           case 7:
             diff = this.registers[(instruction & 0x00f0) >> 4] - this.registers[(instruction & 0x0f00) >> 8]
             borrow = diff < 0
 
-            this.registers[(instruction & 0x0f00) >> 8] = borrow ? diff + 0x100 : diff
+            this.registers[x] = borrow ? diff + 0x0100 : diff
             this.registers[CHIP_8_VF_INDEX] = borrow ? 0 : 1
 
             return this._incrementProgramCounter()
 
           case 0x000e:
-            this.registers[CHIP_8_VF_INDEX] = (this.registers[(instruction & 0x00f0) >> 4] & 0x80) >> 7
-            this.registers[(instruction & 0x0f00) >> 8] = (this.registers[(instruction & 0x00f0) >> 4] << 1) % 0x100
+            this.registers[CHIP_8_VF_INDEX] = (this.registers[y] & 0x0080) >> 7
+            this.registers[x] = (this.registers[y] << 1) % 0x0100
             return this._incrementProgramCounter()
 
           default:
@@ -169,6 +154,11 @@ class Chip8 {
 
   _incrementProgramCounter () {
     this.pc = this.pc + 1
+    return this
+  }
+
+  _jumpIf (condition) {
+    this.pc += condition ? 2 : 1
     return this
   }
 }
